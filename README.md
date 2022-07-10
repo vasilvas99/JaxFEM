666e84b97ba6cb22abeff41969a2687078557166
vasilvas99 <ivanovvasil99@gmail.com>
2022-07-10 17:22:16 +0300

    Finished initial version

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # JaxFEM
 
 An exercise to implement a FEM solver using python and JAX
+
+Currently main.py solves a poisson equation with a right hand side equal to -1:
+
+$\nabla^2 u = -1$
+
+Result is:
+
+![Poisson equation](./plot.png)
--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import FEMcommon.load_mesh as mload
 from FEMcommon.local_matrices import local_stiffness, local_vector
 from FEMcommon.assemble_global import assemble_global_matrix, assemble_global_vector
 from pathlib import Path
+import matplotlib.pyplot as plt
+
 
 def calculate_stiffness_matrix(mesh: mload.Mesh):
     gstiffness = assemble_global_matrix(mesh, local_stiffness)
@@ -25,15 +27,23 @@ def calculate_load_vector(mesh: mload.Mesh):
         gvect[boundary_node] = 0  
     return jnp.array(gvect)
 
+def plot(mesh, values):
+    xs = mesh.nodes[:,0]
+    ys = mesh.nodes[:, 1]
+    zs = values
+    ax = plt.axes(projection='3d')
+    ax.scatter(xs, ys, zs, 'green')
+    plt.savefig("plot.png", dpi=1200)
+
 def main():
-    mesh_json = mload.load_mesh_json(Path("./circle_mesh.json"))
+    mesh_json = mload.load_mesh_json(Path("./circle_fine_mesh.json"))
     mesh = mload.parse_json(mesh_json)
     gstiffness = calculate_stiffness_matrix(mesh)
     gvect = calculate_load_vector(mesh)
     q = jnp.linalg.solve(gstiffness, gvect)
     q = jnp.nan_to_num(q, 0)
-    print(mesh.nodes)
-    print(q)
+    plot(mesh, q)
+
 
 if __name__ == "__main__":
     main()
